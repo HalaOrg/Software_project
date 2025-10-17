e14bc09b085da1c8a4c18a8c63187d10de5af1df
hp <halaeid807@gmail.com>
2025-10-17 19:08:21 +0300

    "Initial commit from IntelliJ"

--- src/main/java/edu/library/Main.java
+++ src/main/java/edu/library/Main.java
@@ -3,5 +3,8 @@ package edu.library;
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
-   
+    //test comment
+    public static void main(String[] args) {
+        System.out.println("Hello, Library!");
+    }
 }
